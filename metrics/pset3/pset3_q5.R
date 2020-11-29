159d8c7ee5ba683c1bd8d935cf75c162628f74fa
Yixin Sun <yixinsun1216@gmail.com>
2020-11-29 11:33:41 +0000

    fix late and mte in q5 pset 3

--- metrics/pset3/pset3_q5.R
+++ metrics/pset3/pset3_q5.R
@@ -79,33 +79,21 @@ estimates <- function(x, y, d, z, single = FALSE){
   ate <- mean(outcomes$y1) - mean(outcomes$y0)
   att <- mean(outcomes$y1[d == 1]) - mean(outcomes$y0[d == 1])
   atu <- mean(outcomes$y1[d == 0]) - mean(outcomes$y0[d == 0])
+  diff <- outcomes$y1_imp - outcomes$y0_imp
 
   if(ncol(z) == 1){
-    #late <-  mean(outcomes$y1[d == 1 & z == 1]) - mean(outcomes$y0[d == 0 & z == 0])
-    late <- (mean(outcomes$y1[z == 1]) - mean(outcomes$y0[z == 0])) / (mean(d[z==1]) - mean(d[z==0]))
+    #late <- (mean(outcomes$y1[z == 1]) - mean(outcomes$y0[z == 0])) / (mean(d[z==1]) - mean(d[z==0]))
+    late <- sum(x$u*diff)/sum(x$u)
   } else{
-    # page 15 here? https://eml.berkeley.edu//~crwalters/papers/multiple_Z.pdf
-    # pz1_1 <- mean(z[,1] == 1)
-    # pz1_0 <- mean(z[,1] == 0)
-    # pz2_1 <- mean(z[,2] == 1)
-    # pz2_0 <- mean(z[,2] == 0)
-    # pz1 <- mean(z[,1])
-    # pz2 <- mean(z[,2])
-    # epz <- mean(pz1_1, pz1_0, pz2_1, pz2_0)
-    # late1 <- (mean(y$worked[z1 == 1]) - mean(y$worked[z1 == 0])) / (mean(d[z1==1]) - mean(d[z1==0]))
-    # late2 <- (mean(y$worked[z2 == 1]) - mean(y$worked[z2 == 0])) / (mean(d[z2==1]) - mean(d[z2==0]))
-    # A <- (pz1_1 - epz)*pz1 + (pz1_0 - epz)*(1-pz1) + (pz2_1 - epz)*pz2 + (pz2_0 - epz)*(1-pz2)
-    # B <- (pz1_1 - pz1_0)*((pz1_1 - epz)*pz1 + (pz2_1 - epz)*pz2 + (pz2_0 - epz)*(1-pz2)) +
-    #   (pz2_0 - pz1_1) * ((pz2_1 - epz)*pz2 + (pz2_0 - epz)*(1-pz2)) +
-    #   (pz2_1 - pz2_0) * (pz2_1 - epz)*pz2
-    # lambda1 <- (pz1_1 - pz1_0)*A / B
-    # lambda2 <- (pz2_1 - pz2_0)*A / B
-    late <- NA # for now!!!
-
+    #late1 <- (mean(outcomes$y1[z[,1] == 1]) - mean(outcomes$y0[z[,1] == 0])) / (mean(d[z[,1]==1]) - mean(d[z[,1]==0]))
+    #late2 <- (mean(outcomes$y1[z[,2] == 1]) - mean(outcomes$y0[z[,2] == 0])) / (mean(d[z[,2]==1]) - mean(d[z[,2]==0]))
+    late1 <- sum(x$u[z[,1] == 1]*diff[z[,1] == 1]) / sum(x$u[z[,1] == 1])
+    late2 <-sum(x$u[z[,2] == 1]*diff[z[,2] == 1]) / sum(x$u[z[,2] == 1])
+    late <- mean(z[,1])/(mean(z[,1]) + mean(z[,2]))*late1 + mean(z[,2])/(mean(z[,1]) + mean(z[,2]))*late2
   }
 
   return(list(ate = ate, att = att, atu = atu, late = late,
-              diff = outcomes$y1_imp - outcomes$y0_imp))
+              m1 = m1, m0 = m0))
 }
 
 # =============================================================================
@@ -114,10 +102,15 @@ estimates <- function(x, y, d, z, single = FALSE){
 run_specifications <- function(z_ind){
   pscore <- probit(bind_cols(xvars, z_ind), dvar)
   d_ind <- pull(dvar, 1)
+  x_mean <- summarise_all(xvars, mean)
+  u_grid <- runif(1000, 0, 1)
 
   # part (i) ----------------------------------------------
   regvars_i <- cbind( data.frame(u = pscore), xvars)
   estimates_i <- estimates(regvars_i, yvar, d_ind, z_ind)
+  mte_i <- map_dbl(u_grid, function(x){
+    vars <- as.matrix(cbind(1, x, x_mean))
+    vars %*% as.matrix(estimates_i$m1$estimate) - vars %*% as.matrix(estimates_i$m0$estimate)})
 
   # part (ii) ----------------------------------------------
   d1 <- d_ind == 1
@@ -125,6 +118,10 @@ run_specifications <- function(z_ind){
   u1 <- data.frame(u1 = matrix(d1*pscore))
   regvars_ii <- cbind(xvars, data.frame(u = pscore), u1, d0 = d0)
   estimates_ii <- estimates(regvars_ii, yvar, d_ind, z_ind, single = TRUE)
+  mte_ii <- map_dbl(u_grid, function(x){
+    vars1 <- as.matrix(cbind(1, x_mean, x, x))
+    vars0 <- as.matrix(cbind(1, x_mean, x, 1))
+    vars1 %*% as.matrix(estimates_ii$m1$estimate) - vars0 %*% as.matrix(estimates_ii$m0$estimate)})
 
   # part (iii) ----------------------------------------------
   # first multiply x with u
@@ -134,15 +131,23 @@ run_specifications <- function(z_ind){
 
   regvars_iii <- cbind( data.frame(u = pscore), xvars, xu)
   estimates_iii <- estimates(regvars_iii, yvar, d_ind, z_ind)
+  mte_iii <- map_dbl(u_grid, function(x){
+    vars <- as.matrix(cbind(1, x, x_mean, x*x_mean))
+    vars %*% as.matrix(estimates_iii$m1$estimate) - vars %*% as.matrix(estimates_iii$m0$estimate)})
 
   # part (iv) ----------------------------------------------
   regvars_iv <- cbind( data.frame(u = pscore, u2 = pscore^2), xvars)
   estimates_iv <- estimates(regvars_iv, yvar, d_ind, z_ind)
+  mte_iv <- map_dbl(u_grid, function(x){
+    vars <- as.matrix(cbind(1, x, x^2, x_mean))
+    vars %*% as.matrix(estimates_iv$m1$estimate) - vars %*% as.matrix(estimates_iv$m0$estimate)})
 
   # part (v) ----------------------------------------------
   regvars_v <- cbind( data.frame(u = pscore, u2 = pscore^2, u3 = pscore^3), xvars)
   estimates_v <- estimates(regvars_v, yvar, d_ind, z_ind)
-
+  mte_v <- map_dbl(u_grid, function(x){
+    vars <- as.matrix(cbind(1, x, x^2, x^3, x_mean))
+    vars %*% as.matrix(estimates_v$m1$estimate) - vars %*% as.matrix(estimates_v$m0$estimate)})
 
   # Put everything into a table ---------------------------
   output <-
@@ -153,21 +158,20 @@ run_specifications <- function(z_ind){
     reduce(left_join)
 
   mte <-
-    list(estimates_i, estimates_ii, estimates_iii, estimates_iv, estimates_v) %>%
+    list(mte_i, mte_ii, mte_iii, mte_iv, mte_v) %>%
     map2_dfr(1:5, function(x, y){
-      diff <- pluck(x, 5)
-      tibble(est = y, mte = diff)
+      tibble(est = y, mte = x)
     }) %>%
-    mutate(u = rep(pscore, 5),
+    mutate(u = rep(u_grid, 5),
            est = paste("Specification", est))
 
   # Graph MTE against u for all specifications -------------
   ggplot(mte) +
     geom_smooth(aes(x = u, y = mte)) +
     theme_minimal() +
-    facet_wrap(~est, ncol = 2)
-  ggsave(file = file.path(gdir, paste0("mte_", paste(colnames(z_ind), collapse = "_"), ".png")),
-         height = 10, width = 7)
+    facet_wrap(~est, ncol = 2, scales = "free")
+  ggsave(file = file.path(gdir, paste0("sunny_mte_", paste(colnames(z_ind), collapse = "_"), ".png")),
+         height = 10, width = 8)
 
   return(output)
 }
