587aeea8d01bd1fc35a6022c5a2422674a8cadfc
Yixin Sun <yixinsun1216@gmail.com>
2020-12-10 11:39:11 +0000

    attempt numero uno for wild bootstraps

--- metrics/pset4/sunny_q2.R
+++ metrics/pset4/sunny_q2.R
@@ -10,10 +10,11 @@ gdir <- "C:/Users/Yixin Sun/Dropbox (Personal)/Coursework/Coursework/metrics/pse
 # =============================================================================
 # Set up functions for simulating and plotting the monte carlos
 # =============================================================================
-M <- 50
+options(warn = -1)
+M <- 100
 t <- 5
 
-ols <- function(x, y){
+ols <- function(x, y, err = "homo", cl = NULL){
   colnames <- c("Constant", colnames(x))
 
   x <- as.matrix(x)
@@ -22,8 +23,63 @@ ols <- function(x, y){
 
   beta <- as.vector(solve(t(x) %*% x, tol = 1e-20) %*% (t(x) %*% y))
 
-  return(tibble(term = colnames, estimate = beta))
+  # find standard errors
+
+  if(err == "homo"){
+    e <- y - x %*% beta
+    se <- sqrt(diag(as.numeric(t(e) %*% e)*solve(t(x) %*% x))/(nrow(x) - ncol(x)))
+  }
+  if(err == "robust"){
+    e <- diag(as.vector(y - x %*% beta)^2, ncol = length(y))
+    meat <- t(x) %*% e %*% x
+    var <- solve(t(x) %*% x) %*% meat %*% solve(t(x) %*% x)
+    se <- sqrt(diag(var))
+  }
+  if(err == "cluster" | err == "wild"){
+    e <- y - x %*% beta
+    se <- cluster_se(x, e, cl)
+  }
+
+  return(tibble(term = colnames, estimate = beta, se = se))
 }
+
+cluster_se <- function(X, e, cl){
+  cluster_vals <- unique(cl)
+
+  sandwich <- matrix(nrow = 0, ncol = ncol(X))
+  for(i in 1:length(cluster_vals)){
+    # filter to a cluster
+    x_g <- matrix(X[which(cl == cluster_vals[i]),], ncol = ncol(X))
+    e_g <- matrix(e[which(cl == cluster_vals[i])], ncol = 1)
+    sandwich <- append(sandwich, list(t(x_g) %*% e_g %*% t(e_g) %*% x_g))
+  }
+
+  vcov <- solve(t(X) %*% X, tol = 1e-20) %*% reduce(sandwich, `+`) %*% solve(t(X) %*% X)
+  se <- sqrt(diag(vcov))
+  return(se)
+}
+
+wild_bootstrap <- function(x, y, cl){
+  cluster_vals <- unique(cl)
+  x_cons <- x[,colnames(x) != "Dr1"]
+  y_cons <- y - sin(1)*x[,colnames(x) == "Dr1"]
+  beta_cons <- as.vector(solve(t(x_cons) %*% x_cons, tol = 1e-20) %*% (t(x_cons) %*% y_cons))
+  u_cons <- y_cons - x_cons %*% beta_cons
+
+  # generate independent Rademacher draws for each cluster
+  rad <- tibble(rad = sample(c(-1, 1), size = length(cluster_vals), replace = TRUE), cluster = cluster_vals)
+  rad_all <- full_join(rad, tibble(cluster = cl), by = "cluster")$rad
+  uwild <- u_cons*rad_all
+  ywild <- x_cons%*%beta_cons + uwild
+
+  beta_wild <- as.vector(solve(t(x) %*% x, tol = 1e-20) %*% (t(x) %*% ywild))
+  e <- ywild - x%*%beta_wild
+  se <- cluster_se(x, e, cl)[colnames(x) == "Dr1"]
+  dr1 <- beta_wild[colnames(x) == "Dr1"]
+  wald <- abs(dr1 - sin(1)) / (se/sqrt(length(y)))
+  return(wald)
+}
+
 
 # setting up all necessary variables
 event_simulation <- function(N, theta = -2, rho = .5, t = 5){
@@ -53,6 +109,9 @@ event_simulation <- function(N, theta = -2, rho = .5, t = 5){
   E <- c(E)
   t_all <- c(t_all)
 
+  # create individual identifiers to use for clustering later
+  indiv <-  c(matrix(rep(1:N, t), ncol = t))
+
   # Construct D so that we can construct Y = D*Y1 + (1-D)*Y0
   D <- as.numeric(E <= t_all)
   Y <- D*Y1 + (1-D)*Y0
@@ -73,7 +132,7 @@ event_simulation <- function(N, theta = -2, rho = .5, t = 5){
     select(-c("Dr-4", "Dr-1"))
 
   xvars <- as.matrix(cbind(cohort_fe, time_fe, reltime_dummies))
-  return(list(xvars, Y, E, t_all))
+  return(list(xvars, Y, E, t_all, indiv))
 }
 
 # using the output from event_simulation() to run OLS
@@ -88,6 +147,7 @@ event_betas <- function(N, theta = -2){
 
 # plotting figures for b and c
 event_plot <- function(B1, B2, theta){
+  # shaping the N = 1K and N=10K output
   output1 <-
     B1 %>%
     as_tibble() %>%
@@ -117,6 +177,7 @@ event_plot <- function(B1, B2, theta){
            N = 10000) %>%
     bind_rows(output1)
 
+  # shaping output from long to wide
   output_mean <- filter(output, stat == "mean")
   output25 <-
     filter(output, stat == "2.5%") %>%
@@ -132,6 +193,7 @@ event_plot <- function(B1, B2, theta){
     full_join(output975) %>%
     mutate(time = as.numeric(time))
 
+  # plotting mean with shaded ribbons for 2.5% and 97.5% values
   ggplot(data = output_wide, aes(x = time, y = value, group = N,
                                  color = as.factor(N))) +
     geom_line() +
@@ -147,30 +209,13 @@ event_plot <- function(B1, B2, theta){
   ggsave(file.path(gdir, paste0('sunny_q2_', theta, ".png")), width = 10, height = 6)
 }
 
-# calculating ATE_3(2) for part (d)
-event_ate <- function(theta, N = 10000, time = 3, event = 2){
-  simulation <- event_simulation(N, theta)
-  Yi <- simulation[[2]]
-  Ei <- simulation[[3]]
-  ti <- simulation[[4]]
-  term1 <- mean(Yi[Ei==event & ti == time])
-  term2 <- mean(Yi[Ei == event & ti == 1])
-  term3 <- mean(Yi[Ei == time + 1 & ti == time]) - mean(Yi[Ei == time + 1 & ti == 1])
 
-  return(term1 - term2 + term3)
-}
-
-# calculating the true value of ATE_3(2)
-true_ate <- function(theta, time = 3, event = 2){
-  sin(time - theta*event)
-}
 
 
 # =============================================================================
-# (B) Plotting figures for theta = -22
+# (B) Plotting figures for theta = -2
 # =============================================================================
 # theta = -2
-plan(multisession, workers = 3)
 
 tic()
 betas_1K <- future_map_dfr(1:M, function(x) event_simulation(1000))
@@ -214,6 +259,24 @@ event_plot(betas_1K, betas_10K, 1)
 # =============================================================================
 # (D) Consistent estimator for ATE(2)
 # =============================================================================
+# calculating ATE_3(2) for part (d)
+event_ate <- function(theta, N = 10000, time = 3, event = 2){
+  simulation <- event_simulation(N, theta)
+  Yi <- simulation[[2]]
+  Ei <- simulation[[3]]
+  ti <- simulation[[4]]
+  term1 <- mean(Yi[Ei==event & ti == time])
+  term2 <- mean(Yi[Ei == event & ti == 1])
+  term3 <- mean(Yi[Ei == time + 1 & ti == time]) - mean(Yi[Ei == time + 1 & ti == 1])
+
+  return(term1 - term2 + term3)
+}
+
+# calculating the true value of ATE_3(2)
+true_ate <- function(theta, time = 3, event = 2){
+  sin(time - theta*event)
+}
+
 ate_all <- map_dbl(c(-2, 0, 1), event_ate)
 ate_true <- map_dbl(c(-2, 0, 1), true_ate)
 
@@ -224,4 +287,53 @@ tibble("$\theta$" = c(-2, 0, 1), Estimate = ate_all, "True ATE" = ate_true) %>%
 # =============================================================================
 # (E) t-tests
 # =============================================================================
-M <- 1000
+# return whether or not the t-stat is greater than 1.96
+event_tstat <- function(N, rho, theta = 1){
+  simulation <- event_simulation(N, theta, rho)
+  err <- c("homo", "robust", "cluster")
+
+  wald_all <-
+    err %>%
+    map(function(x) ols(simulation[[1]], simulation[[2]], x, simulation[[5]])) %>%
+    map(function(x) filter(x, term == "Dr1")) %>%
+    map_dbl(function(x) abs(x$estimate - sin(1)) / (x$se/sqrt(N)) < 1.96)
+
+  wild_ols <- ols(simulation[[1]], simulation[[2]], "wild", simulation[[5]])
+  wald_boot <- map_dbl(1:200, function(b) wild_bootstrap(x, y, cl))
+  wald_boot <- quantile(wald_boot, .95)
+  dr1 <- filter(wild_ols, term == "Dr1")
+  wald_all <- c(wald_all, abs(dr1$estimate - sin(1)) / (dr1$se / sqrt(N)) < wald_boot)
+
+  return(tibble(error = err, reject = wald_all))
+}
+
+# run event_stat() over M simulations to find the proportion of time we reject
+run_tstat <- function(N, rho, M = 1000, theta = 1){
+  print(paste("N =", N, "and rho =", rho))
+  plan(multisession, workers = 3)
+
+  test <-
+    map_dfr(1:M, function(x) tryCatch(event_tstat(N, rho, theta = theta),
+                                      error=function(cond) return(tibble(error = NA, reject = NA)))) %>%
+    filter(!is.na(error)) %>%
+    group_by(error) %>%
+    summarise(reject = mean(reject, na.rm = TRUE)) %>%
+    mutate(N = N, rho = rho)
+  return(test)
+}
+
+rhos <- c(0, .5, 1)
+Ns <- c(20, 50, 200)
+combos <- map(cross2(rhos, Ns), unlist)
+rhos_all <- map(combos, function(x) x[1])
+Ns_all <- map(combos, function(x) x[2])
+
+tic()
+tests <- map2_df(Ns_all, rhos_all, run_tstat)
+toc()
+
+tests_output <-
+  tests %>%
+  mutate(error = factor(error, levels = c("homo", "robust", "cluster"))) %>%
+  arrange(error) %>%
+  spread(key = "rho", value = "reject")
