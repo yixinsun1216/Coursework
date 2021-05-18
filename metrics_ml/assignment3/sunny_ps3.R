7dd8f0b961cadbb78c7ae9d161fd739e6b166f61
Yixin Sun <yixinsun1216@gmail.com>
2021-05-18 14:59:19 +0100

    got the baby tree nice and watered

---
+++ metrics_ml/assignment3/sunny_ps3.R
@@ -0,0 +1,161 @@
+# Created by Yixin Sun on May 18, 2021
+
+library(tidyverse)
+library(tictoc)
+library(knitr)
+library(kableExtra)
+library(furrr)
+gdir <- "C:/Users/Yixin Sun/Dropbox (Personal)/Coursework/Coursework/metrics_ml/assignment3"
+
+# ===================================================================
+# Part A
+# ===================================================================
+data <- tibble(Y = c(0, 1, 1, 0, 1, 0, 1, 1, 0, 0),
+             X = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+sse_gain <- function(Ys, Xs, Is){
+  sum((Ys[Xs <= Is] - mean(Ys[Xs <= Is]))^2) + sum((Ys[Xs > Is] - mean(Ys[Xs > Is]))^2)
+}
+
+tree.grow <- function(data){
+  yi <- as.matrix(data[,1])
+  xi <- as.matrix(data[,-1])
+  N <- nrow(data)
+  p <- ncol(xi)
+
+  gains_all <- matrix(,nrow = N - 1, ncol = 0)
+  for(i in 1:p){
+    gain <- map_dbl(as.matrix(xi[-N,i]), function(x) sse_gain(yi, xi[,i], x))
+    gains_all <- cbind(gains_all, gain)
+  }
+
+  # find index in the gains_all matrix telling me which x variable at what
+  # threshold gives me the greatest gain in SSE
+  xstar_ij <- which(gains_all == min(gains_all), arr.ind = TRUE)[1,]
+
+  # return dataframe of best splitting criterion, decrease in SSE, and partition
+  # of Ys denoted by (0,1) under the criterion
+  output <- tibble(
+    kstar = rep(xstar_ij[2], N),
+    xstar = rep(xi[xstar_ij[1], xstar_ij[2]], N),
+    sse_gain = rep(min(gains_all), N),
+    partition = xi[,xstar_ij[2]] <= xi[xstar_ij[1], xstar_ij[2]]
+  )
+
+  return(output)
+}
+
+# ===================================================================
+# Part B
+# ===================================================================
+tree.update <- function(data, split, assign, min.size, max.depth){
+  # for leave with depth less than max.depth and size bigger than min.size,
+  # use tree.grow to find best splitting
+  data_assigned <-
+    cbind(data, assign) %>%
+    group_by(term_id) %>%
+    mutate(end = !(depth < max.depth & n() > min.size))
+
+  if(sum(data_assigned$end) == nrow(data)){
+    # If there's no more leaves to grow, end here
+    return("end")
+  } else{
+
+    # if there are still leaves to grow, throw each set of terminal xs into tree.grow
+    # split data into list based on term_id
+    data_list <-
+      data_assigned %>%
+      filter(!end) %>%
+      split(f = .$term_id)
+
+    data_grow <- map(data_list, function(x) tree.grow(x[, 1:ncol(data)]))
+
+    # loop through each group to figure out the one that returns the greatest
+    # gains to sse
+    best_index <-
+      map_dbl(data_grow, function(x) unique(x$sse_gain)) %>%
+      which.min
+
+    best_grow <- cbind(data_grow[[best_index]], data_list[[best_index]])
+
+    # now update split and assign
+    # for the split matrix, we want to add a new row
+    split_updated <- rbind(split, c(best_grow$term_id[1], best_grow$depth[1], best_grow$kstar[1], best_grow$xstar[1]))
+
+    # for assign, we want the xs that were in the terminal node that we split on
+    # to get updated term_ids
+    assign_updated <-
+      assign %>%
+      left_join(select(best_grow, -term_id, -depth)) %>%
+      mutate(term_id = case_when(
+        partition ~ 2*term_id,
+        !partition ~ 2*term_id + 1,
+        is.na(partition) ~ term_id),
+        depth = if_else(is.na(partition), depth, depth + 1)) %>%
+      select(id, term_id, depth)
+
+
+    return(list(split = split_updated,
+                assign = assign_updated))
+  }
+}
+
+
+# ===================================================================
+# Part C
+# ===================================================================
+tree <- function (data, min.size = 10, max.depth = 10){
+  # use tree.grow on whole dataset to get the first split
+  grow1 <- tree.grow(data)
+  split1 <- tibble(leaf_id = 1, depth = 0, kstar = grow1$kstar[1], threshold = grow1$xstar[1])
+  assign1 <-
+    grow1 %>%
+    mutate(term_id = if_else(partition, 2, 3),
+           depth = 1,
+           id = 1:nrow(data)) %>%
+    select(term_id, depth, id)
+
+  updated_tree <- list(split = split1, assign = assign1)
+
+  while(updated_tree != "end"){
+    assign <- updated_tree$assign
+    split <- updated_tree$split
+    updated_tree <- tree.update(data, split, assign, min.size, max.depth)
+  }
+
+  # compute leaves, which are the id, depth, size, and mean of the terminal leaves
+  leaves <-
+    cbind(data, assign) %>%
+    group_by(leaf_id = term_id, depth) %>%
+    summarise(size = n(),
+              Ymean = mean(Y))
+
+  return(list(split = split, assign = assign, leaves = leaves))
+}
+
+# ===================================================================
+# Part D
+# ===================================================================
+tree.predict <- function(data, tree){
+  # for each Y, move through rows of split matrix  and categorize the Y variable
+  for(j in 1:nrow(data)){
+    yj <- data[j, 1]
+    xj <- data[j, -1]
+
+  }
+}
+
+
+
+# ===================================================================
+# Part E
+# ===================================================================
+N <- 1000
+X1 <- rnorm(N, 0, 1)
+X2 <- rnorm(N, 0, 1)
+eps <- rnorm(N, 0, 1)
+
+Y <- 3 * min(X1, X2) + eps
+
+# what am i cross-validating here if i'm not trying to figure out the optimal tuning parameter?
+
